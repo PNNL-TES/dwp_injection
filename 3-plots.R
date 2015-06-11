dc85c13dc9a8538a5b5a29b50f8853c735ec05f9
Ben Bond-Lamberty <bondlamberty@pnnl.gov>
2015-06-11 10:34:36 -0400

    Corrected valve-core mapping
    Per work by, and conversation with, Sarah Fansler

--- 3-plots.R
+++ 3-plots.R
@@ -20,6 +20,8 @@ summarydata$DATETIME <- ymd_hms(summarydata$DATETIME)
 print_dims(summarydata)
 print(summary(summarydata))
 
+summarydata <- subset(summarydata, CH4_dry <= 3.0)
+
 p1 <- ggplot(summarydata, aes(DATETIME, CO2_dry, color=DWP_core)) + geom_point()
 p1 <- p1 + facet_grid(Source~Rep, scales="free")
 p1 <- p1 + theme(axis.text.x = element_text(angle = 90))
@@ -60,9 +62,19 @@ for(dwpc in unique(summarydata$DWP_core)) {
   p <- p + facet_grid(variable~Trt, scales="free")
   p <- p + ggtitle(paste("DWP core", dwpc)) + xlab("Elapsed time (hours)")
   print(p)
-  save_plot(paste0("DWP_", dwpc))
+  save_plot(paste0("DWP_core_", dwpc))
 }
 
+printlog("Plotting individual valves...")
+for(dwpv in unique(summarydata$Valve)) {
+  printlog("Plotting", dwpv)
+  d <- subset(summarydata, Valve==dwpv) %>% melt(measure.vars=c("CO2_dry", "CH4_dry"))
+  p <- ggplot(d, aes(ELAPSED_TIME/60/60, value, color=Rep)) + geom_point()
+  p <- p + facet_grid(variable~Trt, scales="free")
+  p <- p + ggtitle(paste("DWP valve", dwpv)) + xlab("Elapsed time (hours)")
+  print(p)
+  save_plot(paste0("DWP_valve_", dwpv))
+}
 
 printlog("All done with", SCRIPTNAME)
 print(sessionInfo())
